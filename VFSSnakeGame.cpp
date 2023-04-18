2181628937b3f8e0a186832839dd8f07f24bb475
pg24gavin <pg24gavin@vfs.com>
2023-04-18 14:18:06 -0700

    base stuff (might be buggy)

--- VFSSnakeGame.cpp
+++ VFSSnakeGame.cpp
@@ -1,20 +1,94 @@
-// VFSSnakeGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <conio.h>
+#include <ctime>
+
+#include "Snake.h"
+#include "Food.h"
+
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+Snake snake({ WIDTH / 2, HEIGHT / 2 }, 1);
+Food food;
+
+int score;
+
+void board()
+{
+    COORD snake_pos = snake.get_pos();
+    COORD food_pos = food.get_pos();
+
+    vector<COORD> snake_body = snake.get_body();
+
+    cout << "SCORE : " << score << "\n\n";
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        cout << "\t\t#";
+        for (int j = 0; j < WIDTH - 2; j++)
+        {
+            if (i == 0 || i == HEIGHT - 1) cout << '#';
+
+            else if (i == snake_pos.Y && j + 1 == snake_pos.X) cout << '0';
+            else if (i == food_pos.Y && j + 1 == food_pos.X) cout << '@';
+
+            else
+            {
+                bool isBodyPart = false;
+                for (int k = 0; k < snake_body.size() - 1; k++)
+                {
+                    if (i == snake_body[k].Y && j + 1 == snake_body[k].X)
+                    {
+                        cout << 'o';
+                        isBodyPart = true;
+                        break;
+                    }
+                }
+
+                if (!isBodyPart) cout << ' ';
+            }
+        }
+        cout << "#\n";
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    score = 0;
+    srand(time(NULL));
+
+    food.gen_food();
+
+    char game_over = false;
+
+    while (!game_over)
+    {
+        board();
+
+        if (kbhit())
+        {
+            switch (getch())
+            {
+            case 'w': snake.direction('u'); break;
+            case 'a': snake.direction('l'); break;
+            case 's': snake.direction('d'); break;
+            case 'd': snake.direction('r'); break;
+            }
+        }
+
+        if (snake.collided()) game_over = true;
+
+        if (snake.eaten(food.get_pos()))
+        {
+            food.gen_food();
+            snake.grow();
+            score = score + 10;
+        }
+
+        snake.move_snake();
+
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
